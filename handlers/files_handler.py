ced61fd60fdfe873dd9cb91becc84be31063a9db
Pedro Heleno Isolani <pedroisolani@gmail.com>
2018-08-01 10:02:08 +0200

    stats file format changed

--- handlers/files_handler.py
+++ handlers/files_handler.py
@@ -20,7 +20,7 @@ def initialize_stats_files():
     try:
         live_capture_logger.info('Initializing WTPs stats files...')
         # TODO iterate over WTPs list
-        wtp_aggregated_stats_file = open('stats/wtp_aggregated_stats_' + str(socket.gethostname()) + '.json', 'w+')
+        wtp_aggregated_stats_file = open('stats/' + str(socket.gethostname()) + '.aggr.json', 'w+')
         wtp_aggregated_stats_file.write(str(json.dumps(WTPAggregatedStats().get(),
                                                        default=lambda o: o.__dict__['data'])))
         wtp_aggregated_stats_file.close()
@@ -33,7 +33,7 @@ def initialize_stats_files():
 def add_wtp_raw_stats_to_file(wtp_raw_stats):
     try:
         live_capture_logger.info('Adding WTP raw stats into file...')
-        wtp_raw_stats_file = open('stats/wtp_raw_stats_' + str(socket.gethostname()) + '.json', 'w+')
+        wtp_raw_stats_file = open('stats/' + str(socket.gethostname()) + '.raw.json', 'w+')
         wtp_raw_stats_file.write(str(json.dumps(wtp_raw_stats.get())))
         wtp_raw_stats_file.close()
     except:
@@ -45,7 +45,7 @@ def add_wtp_raw_stats_to_file(wtp_raw_stats):
 def add_wtp_aggregated_stats_to_file(wtp_aggregated_stats):
     try:
         live_capture_logger.info('Adding WTP aggregated stats into file...')
-        wtp_aggregated_stats_file = open('stats/wtp_aggregated_stats_' + str(socket.gethostname()) + '.json', 'w+')
+        wtp_aggregated_stats_file = open('stats/' + str(socket.gethostname()) + '.aggr.json', 'w+')
         wtp_aggregated_stats_file.write(str(json.dumps(wtp_aggregated_stats, default=lambda o: o.__dict__['data'])))
         wtp_aggregated_stats_file.close()
     except:
