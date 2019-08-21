25da177d5a7980c0c973f1f1bde5e22d4b1a0008
Pedro Heleno Isolani <pedroisolani@gmail.com>
2019-08-21 13:15:54 +0200

    CPU and memory script minor fix

--- shell_scritps/performance_monitor.sh
+++ shell_scritps/performance_monitor.sh
@@ -9,7 +9,7 @@ do
         esac
 done
 
-echo "Time (sec), CPU Load (%), Memory (%), Process Name, PID" > cpu_memory_output.txt
+echo "Time (sec), CPU Load (%), Memory (%), Process Name, PID" > cpu_memory_output.csv
 
 top -b -d 1 -p $PID n $TIMEOUT | awk -v OFS="," '$1+0>0 {print $9,$10,$NF,$1; fflush() }' >> raw_output.txt
 
